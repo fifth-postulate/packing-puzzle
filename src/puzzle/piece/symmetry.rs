397117cbfa6c0799dfe0eb2d752227adea5e1570
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-09 15:34:21 +0100

    Implement Transformable for triples and tuples

--- src/puzzle/piece/symmetry.rs
+++ src/puzzle/piece/symmetry.rs
@@ -112,3 +112,52 @@ impl Iterator for CubeSymmetryIterator {
         }
     }
 }
+
+impl Transformable for (i8, i8, i8) {
+    fn transform(&mut self, symmetry: &CubeSymmetry) {
+        let x = self.0;
+        let y = self.1;
+        let z = self.2;
+        let sx: i8;
+        let sy: i8;
+        let sz: i8;
+        match *symmetry {
+            CubeSymmetry::E0123 => { sx =  x; sy =  y; sz =  z; },
+            CubeSymmetry::E0132 => { sx = -z; sy = -y; sz = -x; },
+            CubeSymmetry::E0213 => { sx = -x; sy = -z; sz = -y; },
+            CubeSymmetry::E0231 => { sx =  y; sy =  z; sz =  x; },
+            CubeSymmetry::E0312 => { sx =  z; sy =  x; sz =  y; },
+            CubeSymmetry::E0321 => { sx = -y; sy = -x; sz = -z; },
+            CubeSymmetry::E1023 => { sx =  z; sy = -y; sz =  x; },
+            CubeSymmetry::E1032 => { sx = -x; sy =  y; sz = -z; },
+            CubeSymmetry::E1203 => { sx = -z; sy = -x; sz =  y; },
+            CubeSymmetry::E1230 => { sx = -y; sy =  x; sz =  z; },
+            CubeSymmetry::E1302 => { sx =  x; sy =  z; sz = -y; },
+            CubeSymmetry::E1320 => { sx =  y; sy = -z; sz = -x; },
+            CubeSymmetry::E2013 => { sx = -y; sy =  z; sz = -x; },
+            CubeSymmetry::E2031 => { sx =  x; sy = -z; sz =  y; },
+            CubeSymmetry::E2103 => { sx =  y; sy =  x; sz = -z; },
+            CubeSymmetry::E2130 => { sx =  z; sy = -x; sz = -y; },
+            CubeSymmetry::E2301 => { sx = -x; sy = -y; sz =  z; },
+            CubeSymmetry::E2310 => { sx = -z; sy =  y; sz =  x; },
+            CubeSymmetry::E3012 => { sx =  y; sy = -x; sz =  z; },
+            CubeSymmetry::E3021 => { sx = -z; sy =  x; sz = -y; },
+            CubeSymmetry::E3102 => { sx = -y; sy = -z; sz =  x; },
+            CubeSymmetry::E3120 => { sx = -x; sy =  z; sz =  y; },
+            CubeSymmetry::E3201 => { sx =  z; sy =  y; sz = -x; },
+            CubeSymmetry::E3210 => { sx =  x; sy = -y; sz = -z; },
+         }
+        self.0 = sx;
+        self.1 = sy;
+        self.2 = sz;
+    }
+}
+
+impl Transformable for (i8, i8) {
+    fn transform(&mut self, symmetry: &CubeSymmetry) {
+        let mut v: (i8, i8, i8) = (self.0, self.1, 0);
+        v.transform(symmetry);
+        self.0 = v.0;
+        self.1 = v.1;
+    }
+}
